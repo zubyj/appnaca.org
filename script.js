7518d976f963b13bdf3dfd81d00d0a7198b4a5d2
zubyjaved <zubyjaved@gmail.com>
2022-01-12 02:15:26 -0500

    move google map script to html page

--- script.js
+++ script.js
@@ -8,33 +8,6 @@ $(function(){
         $('nav').addClass('sticky-top')
     }
 
+
+
 });
-
-// Initialize and add the map
-function initMap() {
-    const norcal = { lat: 37.34017224895144, lng: -121.78553724593377 };
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 8,
-      center: norcal,
-    });
-    // The marker, positioned at Uluru
-    const marker = new google.maps.Marker({
-      position: norcal,
-      map: map,
-    });
-  }
-
-// let email = document.getElementById("email");
-// // Copies the email to clipboard on click.
-// email.onclick = function() {
-//     const el = document.createElement("textarea");
-//     el.value = email.innerText;
-//     document.body.appendChild(el);
-//     el.select();
-//     document.execCommand("copy")
-//     document.body.removeChild(el);
-//     oldText = email.innerHTML
-//     email.innerHTML = "<span style='color: black;'>Copied to clipboard!</span>";
-//     setTimeout(() => {  email.innerHTML = oldText; }, 3000);
-// };
-
