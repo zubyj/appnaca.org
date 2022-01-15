a2714ebfc311c8f6bff1b7f6365bcde6b565b527
zubyjaved <zubyjaved@gmail.com>
2022-01-15 14:27:55 -0500

    add & submit ajax form w/jquery

--- script.js
+++ script.js
@@ -7,4 +7,27 @@ $(function(){
     else {
         $('nav').addClass('sticky-top')
     }
+
+
+
+    $("form").on(function(event) {
+        var formData = {
+            name: $("#inputName").val(),
+            email: $("#inputEmail").val(),
+            message: $("#inputMessage").val()
+        };
+        $.ajax({
+            type: "POST",
+            url: "appnaca.org:1000",
+            data: formData,
+            dataType: "json",
+            encode: true,
+        }).done (function(data) {
+            console.log(data);
+        });
+
+        // prevent form from reloading page on submission.
+        event.preventDefault();
+    });
 });
+
